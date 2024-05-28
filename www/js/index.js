d5bd57f1832571a0b658514fb11f8a2568ad7ba2
eihwan-sfdc <eihwan.kim@salesforce.com>
2024-05-28 18:15:59 +0900

    modified index.js like MC Plugin Sample

--- www/js/index.js
+++ www/js/index.js
@@ -1,67 +1,45 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var app = {
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+    // 初期化(このJSの一番下で呼ばれる)
+    initialize: function() {
+        document.addEventListener(
+            'deviceready', this.onDeviceReady.bind(this), false);
+    },
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    // 各種紐づけ
+    onDeviceReady : function () {
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-    // $(".app").hide();
-    // $("#page-firstpage").show();
-    // var contactKey = MCCordovaPlugin.getContactKey();
-    // $("#input-contactKey").text(contactKey);
-    
-    MCCordovaPlugin.setOnNotificationOpenedListener(function(value) {
+        console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+        document.getElementById('deviceready').classList.add('ready');
+
+        MCCordovaPlugin.getContactKey(app.handleContactKey);
+        MCCordovaPlugin.setOnNotificationOpenedListener(app.notificationOpened);
+
+        document.getElementById('setContactKey')
+            .addEventListener('click', app.contactKeySubmit);
+        document.getElementById('logSdkState')
+            .addEventListener('click', app.logSdkState);
+
+        
+    }, 
+
+    // ここから下が実際の処理
+    handleContactKey: function(key) {
+        document.getElementById('contactKey').value = key;
+    },
+    contactKeySubmit: function() {
+        MCCordovaPlugin.setContactKey(
+            document.getElementById('contactKey').value);
+    },
+    logSdkState : function () {
+        MCCordovaPlugin.logSdkState();
+    },
+    notificationOpened : function (value) {
         var jsonString = JSON.stringify(value, null, 4);
         console.log(jsonString);
-    });
-}
+    },
 
-// page-firstpage
-$(function(){
 
-    $("#button-log").on('click', function(){
-        MCCordovaPlugin.logSdkState(function(contactKey){
-        }, console.log('MCCordovaPlugin.logSdkState failed'));
-    });
-    
-    $("#button-getDeviceId").on('click', function(){
-        MCCordovaPlugin.getDeviceId(function(deviceId){
-            alert(deviceId);
-        }, console.log('MCCordovaPlugin.getDeviceId failed'));
-    });
-    
-    $("#button-getContactKey").on('click', function(){
-        MCCordovaPlugin.getContactKey(function(contactKey){
-            alert(contactKey);
-        }, console.log('MCCordovaPlugin.getContactKey failed'));
-    });
-    
-    $("#button-setContactKey").on('click', function(){
-        MCCordovaPlugin.setContactKey('cordovaContactKey', function(success){
-            console.log(success);
-        }, console.log('MCCordovaPlugin.setContactKey failed'));
-    });
+};
 
-});
-
+app.initialize();
